53a546a957619034847b4fe515eb44fa75d72d6f
MAH1UZ <mah1uzahmed@gmail.com>
2026-09-08 13:39:58 +0600

    functions

--- js_practice/test1.js
+++ js_practice/test1.js
@@ -1,6 +1,17 @@
-console.log('Hello World!')
+// console.log('Hello World!')
 //this is a comment
 /* this is also
  a comment
 
 */
+
+function myCar(){
+    wheel: 4;
+    color: 'black';
+    driver_seat: 1;
+    var arrMy=['wheel', 'color', 'driver_seat'];
+    return arrMy;
+}
+
+console.log(myCar());
+
